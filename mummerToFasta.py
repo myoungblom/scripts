1f7c1bde1a2824c1138fbc0dd12b9237bf64b756
Madison Youngblom <youngblom@wisc.edu>
2019-03-06 15:23:28 -0600

    writing script

--- mummerToFasta.py
+++ mummerToFasta.py
@@ -5,9 +5,10 @@ from Bio import SeqIO
 
 
 #######
-#This script takes in the output from mummer/nucmer 'show-snps -T' and creates a fasta alignment
-#using the reference sequence used to call the snps with nucmer.
-#
+# This file takes in mummer output from 'show-snps -T' and produces a fasta sequence
+# using the reference sequence used to call the snps with nucmer.
+# ** will ignore insertions **
+# ** outputs single contig, named identically to 'outputfile' name given as argument **
 #######
 
 # check for correct command line arguments
@@ -25,6 +26,14 @@ RefFile = open(RefName, 'r') #read reference file
 OutFile = open(OutFileName, 'w') #write output file
 
 # read reference sequence
+# write entire reference sequence to output file?
+# 	or write reference as you go until you hit position in column1?
+# for row in columns
+#	if column2 == '.' skip
+#	else
+#	change position column1 to column3 in reference
+#		if letter change to letter
+#		if '.' change to '-'
 # edit reference sequence based on SNPs/gaps in mummer output
 # ignore insertions compared to reference
 # write new sequence in fasta format
