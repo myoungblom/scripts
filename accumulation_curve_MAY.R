87ce54359aec8666cd78ef302b1ef47e6512ca40
Madison Youngblom <youngblom@wisc.edu>
2020-02-07 09:35:41 -0600

    minor changes

--- accumulation_curve_MAY.R
+++ accumulation_curve_MAY.R
@@ -18,17 +18,14 @@
 # =========================================================
 
 #Define working directory
-setwd("~/Desktop/");
+setwd("~/Desktop/Pepperell/saprophyticus/2019.07.31_saproRarefaction");
 
-#read the input file. 
+#read the FIRST input file - the one to be subsampled. 
 #The input file must have no header row and column.
 #rows corespond to genes, columns corespond to isolates
 data <- read.table("clade1_gpa_reduced_rarefaction.tsv", sep="\t");
 transposed_data <- t(data);#we transpose the table to run a for loop row-wise
-#write.table(transposed_data, file = "transposed_data.txt", sep="\t")
-subset <- transposed_data[sample(nrow(transposed_data), 17), ]
-#write.table(subset, file = "subset_data.txt", sep="\t")
-
+subset <- transposed_data[sample(nrow(transposed_data), 17), ] # 17 == the number of isolates to be sampled from larger pop
 nr_rows <- nrow(subset);
 nr_cols <- ncol(subset);
 nr_iterations <- 100;#the number of iterations
@@ -74,14 +71,16 @@ for(times in 1: nr_iterations){
   subset <- transposed_data[sample(nrow(transposed_data), 17), ]
 }
 
-result <- t(core_results)
+# stores first results
+result3 <- t(core_results)
+result3 <- as.data.frame(result3)
 
-#read the input file. 
+#read the SECOND input file - this one isnt subsampled. 
 #The input file must have no header row and column.
 #rows corespond to genes, columns corespond to isolates
 data2 <- read.table("clade2_gpa_reduced_rarefaction.tsv", sep="\t");
 transposed_data2 <- t(data2);#we transpose the table to run a for loop row-wise
-nr_rows2 <- nrow(transposed_data2);
+nr_rows2 <- nrow(transposed_data2);# this file isn't sampled, just run as whole
 nr_cols2 <- ncol(transposed_data2);
 nr_iterations <- 100;#the number of iterations
 
@@ -126,17 +125,20 @@ for(times in 1: nr_iterations){
   transposed_data2 <- transposed_data2[sample(nrow(transposed_data2), nrow(transposed_data2)), ]
 }
 
-result2 <- t(core_results2)
+# stores second results
+result4 <- t(core_results2)
+result4 <- as.data.frame(result4)
 
-#print the results in a tab delemeter file
-#write.table(core_results, file = "pan_genome.txt", sep="\t");
-
+# plots first results
 clade1 <- boxplot(result, data=result, xlab="Number of genomes", ylab="Number of genes in pangenome",
         varwidth=TRUE, outline=FALSE, ylim=c(2000,6000), col = alpha("pink2",0.8))
+# adds grid for visibility
 grid()
-
+# plots first plot over grid
 boxplot(result, data=result,varwidth=TRUE, outline=FALSE,col = alpha("pink2",0), add=TRUE)
-
+# plots second plot
 clade2<- boxplot(result2, data=result2, varwidth=TRUE, ylim=c(2000,6000), 
                  outline=FALSE, add = TRUE, col = alpha("Lightskyblue", 0.8))
+
+# adds legend
 legend("topleft", legend = c("Clade 1", "Clade 2"), col = c("pink2", "lightskyblue"), pch = 15)
--- rarefaction_curve_MAY.R
+++ rarefaction_curve_MAY.R
@@ -14,17 +14,18 @@
 # identification of novel epidemiological markers in 
 # pathogenic Campylobacter
 # =========================================================
-
+# Edited by Madison Youngblom 08/2019
+# =========================================================
 
 #Define working directory
-setwd("~/Desktop/");
+setwd("~/Desktop/Pepperell/saprophyticus/2019.07.31_saproRarefaction");
 
-#read the input file. 
+#read the FIRST input file - the one to be subsampled. 
 #The input file must have no header row and column.
 #rows corespond to genes, columns corespond to isolates
 data <- read.table("clade1_gpa_reduced_rarefaction.tsv", sep="\t");
 transposed_data <- t(data);#we transpose the table to run a for loop row-wise
-subset <- transposed_data[sample(nrow(transposed_data), 17), ]
+subset <- transposed_data[sample(nrow(transposed_data), 17), ] # 17 == the number of isolates to be sampled from larger pop
 nr_rows <- nrow(subset);
 nr_cols <- ncol(subset);
 nr_iterations <- 100; #the number of iterations 
@@ -70,17 +71,16 @@ for(times in 1:nr_iterations){
   subset <- transposed_data[sample(nrow(transposed_data), 17), ]
 }
 
-#print the results in a tab delemeter file
-#write.table(core_results, file = "core_genome.txt", sep="\t");
-
+# results from first file to be plotted
 result <- t(core_results)
+result <- as.data.frame(result)
 
-#read the input file. 
+#read the SECOND input file - this one is not subsampled. 
 #The input file must have no header row and column.
 #rows corespond to genes, columns corespond to isolates
 data2 <- read.table("clade2_gpa_reduced_rarefaction.tsv", sep="\t");
 transposed_data2 <- t(data2);#we transpose the table to run a for loop row-wise
-nr_rows2 <- nrow(transposed_data2);
+nr_rows2 <- nrow(transposed_data2); # this file isn't sampled, just run as whole
 nr_cols2 <- ncol(transposed_data2);
 nr_iterations <- 100; #the number of iterations 
 
@@ -125,18 +125,73 @@ for(times in 1:nr_iterations){
   transposed_data2 <- transposed_data2[sample(nrow(transposed_data2), nrow(transposed_data2)), ]
 }
 
-#print the results in a tab delemeter file
-#write.table(core_results, file = "core_genome.txt", sep="\t");
+# results from second file
 result2 <- t(core_results2)
-
-
+result2 <- as.data.frame(result2)
+avg2 <- col
+# this plots the first results
 clade1 <- boxplot(result, data=result, xlab="Number of genomes", ylab="Number of genes in core genome",
                   varwidth=TRUE, outline=FALSE, ylim=c(2000,2800), col = alpha("pink2",0.8))
+clade_colors <- c("pink2", "lightskyblue")
+
+# plotting conserved and total genes for each clade
+library(ggplot2)
+conserved1 = apply(result, 2, median) # rarefaction results from clade 1
+conserved2 = apply(result2, 2, median) # rarefaction results from clade 2
+total1 = apply(result3, 2, median) # accumulation results from clade 1
+total2 = apply(result4, 2, median) # accumulation results from clade 2
+
+genes1 = data.frame( genes_to_genomes1 = c(conserved1,total1),
+                     genomes1 = c(c(1:length(conserved1)),c(1:length(conserved1))),
+                     Key = c(rep("Core genes",length(conserved1)), rep("Total genes",length(total1))) )
+genes2 = data.frame( genes_to_genomes2 = c(conserved2,total2),
+                     genomes2 = c(c(1:length(conserved2)),c(1:length(conserved2))),
+                     Key = c(rep("Core genes",length(conserved2)), rep("Total genes",length(total2))) )
+
+setwd("~/Desktop/2019_MWPG/figures/rarefaction/")
+
+rarefaction <- ggplot() +geom_line(data = genes1, aes(x = genomes1, y = genes_to_genomes1, group = Key, linetype=Key, color = "Clade 1"), size = 1.5, alpha = 0.8) +
+  geom_line(data = genes2, aes(x = genomes2, y = genes_to_genomes2, group = Key, linetype=Key, color = "Clade 2"), size = 1.5, alpha = 0.8)+
+  scale_color_manual("Clade", breaks = c("Clade 1", "Clade 2"), values = c("Pink2", "Lightskyblue"))+
+  theme_classic() +
+  labs(linetype = "Genes")+
+  ylim(c(2000,max(total1)))+
+  xlim(c(1,length(total1)))+
+  xlab("\nNumber of genomes") +
+  ylab("Number of genes\n")+ theme_bw(base_size = 10) +
+  theme(legend.justification=c(0,1),legend.position=c(0.01,.99), legend.box = "vertical",
+        legend.text = element_text(color = "grey27", size = 12), 
+        legend.title = element_text(color = "grey27", face = "bold"),
+        axis.text = element_text(color = "grey27", face = "bold"),
+        axis.title = element_text(color = "grey27", face = "bold", size = 12))+
+  guides( color = guide_legend(order = 1), linetype = guide_legend(order = 2))
+
+#function to export plot
+ExportPlot <- function(gplot, filename, width=2, height=1.5) {
+  # Export plot in PDF and EPS.
+  # Notice that A4: width=11.69, height=8.27
+  ggsave(paste(filename, '.pdf', sep=""), gplot, width = width, height = height)
+  postscript(file = paste(filename, '.eps', sep=""), width = width, height = height, family = "sans")
+  print(gplot)
+  dev.off()
+  png(file = paste(filename, '_.png', sep=""), width = width * 100, height = height * 100)
+  print(gplot)
+  dev.off()
+}
+
+#export plot
+setwd("/Users/madi/Desktop/2019_MWPG/figures/rarefaction/")
+ExportPlot(rarefaction, "conserved_vs_total_genes_median", width=6, height=8)
+
+
+
+# adds a grid for visibility
 grid()
-
+# replots the first plot over the grid
 boxplot(result, data=result,varwidth=TRUE, outline=FALSE,col = alpha("pink2",0), add=TRUE)
-
+# plots the second results
 clade2<- boxplot(result2, data=result2, varwidth=TRUE, ylim=c(2000,max(result2)), 
                  outline=FALSE, add = TRUE, col = alpha("Lightskyblue", 0.8))
+# makes a legend
 legend("topright", legend = c("Clade 1", "Clade 2"), col = c("pink2", "lightskyblue"), pch = 15)
 
