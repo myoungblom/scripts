597f327cab48c3d6341618f5048c4c0d13df0ea7
Madison Youngblom <youngblom@wisc.edu>
2020-03-04 09:53:54 -0600

    changes to format converters

---
+++ IGRgff.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+
+#####
+# This script takes a GFF and outputs a GFF with added intergenic regions.
+#####
+
+# check for correct arguments
+if len(sys.argv) != 2:
+	print("Usage: IGRgff.py <gff>")
+	sys.exit(0)
+
+gff = sys.argv[1]
+out = gff.strip(".gff")+"_IGR.gff"
+output = open(out, "w")
+geneDict = {}
+
+# get end of first coding region
+with open(gff, "r") as f:
+	for line in f:
+		line = line.strip("\n")
+		if not line.startswith("#"):
+			parser = line.split("\t")
+			feature = parser[2]
+			end = parser[4]
+			if feature == "gene":
+				IGRstart = int(end)
+				break
+
+with open(gff, "r") as f:
+	counter = 1
+	for line in f:
+		line = line.strip("\n")
+		if not line.startswith("#"):
+			parser = line.split("\t")
+			feature = parser[2]
+			start = parser[3]
+			end = parser[4]
+			if feature == "gene" and start != "1":
+				IGRend = int(start)-1
+				output.write("\t".join(parser[:2])+"\tIGR\t"+str(IGRstart)+"\t"+str(IGRend)+"\t.\t+\t.\tID=IGR"+str(counter)+";Name=IGR_"+str(IGRstart)+"_"+str(IGRend)+"\n")
+				IGRstart = int(end)+1
+				counter += 1
+		output.write(line+"\n")
