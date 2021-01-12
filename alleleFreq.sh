ab1ff37612cee24d2ef3fd3d78b91910104f23ce
Madison Youngblom <madisonyoungblom@static-214-209.vpn.wisc.edu>
2021-01-12 08:53:43 -0600

    testing

--- alleleFreq.sh
+++ alleleFreq.sh
@@ -1,6 +1,27 @@
 #! bin/bash
 
+#####
+# This script serves the same function as breseq and gdtools compare - input is a 
+# a reference, a bed file of regions to exclude and a set of bams
+# from a single strain sequenced at different time points. Output is a csv of mutation frequencies
+# across those time points. Also requires companion python script "alleleFreq.py"
+#####
 
-bcftools mpileup -f /opt/data/mtuberculosis/MtbNCBIH37Rv.fa -o test.vcf -O v -a AD,ADF,ADR ERR003120.realn.bam
+# usage: alleleFreq.sh <path.to.ref> <bed> <bam1> <bam2> ... <bamN>
 
-bcftools filter -s LowQual -e "%QUAL<20 || DP>100" test.vcf > test.flt.vcf
+ref="$1"
+bed="$2"
+bams="${@:3}"
+
+# compile mutations from bam files
+for x in ${bams}
+do
+    bcftools mpileup -f ${ref} -o ${x/.bam/.vcf} -O v -a AD,ADF,ADR ${x}
+done
+
+# filter out low quality mutations
+for x in ${bams}
+do
+    bcftools filter -s LowQual -e "%QUAL<20 || DP>100" ${x/.bam/.vcf} > ${x/.vcf/.flt.vcf}
+done
+
