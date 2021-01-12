5d0d561385ba0a2ff67dfd6396f3353c25a950bc
Madison <youngblom@wisc.edu>
2021-01-12 08:35:54 -0600

    downloads assemblies from NCBI

---
+++ AssemblyDownload.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import urllib
+
+def get_assembly_summary(id):
+    """Get esummary for an entrez id"""
+    from Bio import Entrez
+    esummary_handle = Entrez.esummary(db="assembly", id=id, report="full")
+    esummary_record = Entrez.read(esummary_handle)
+    return esummary_record
+
+def get_assemblies(term, download=True, path='assemblies'):
+    """Download genbank assemblies for a given search term.
+    Args:
+        term: search term, usually organism name
+        download: whether to download the results
+        path: folder to save to
+    """
+
+    from Bio import Entrez
+    #provide your own mail here
+    Entrez.email = "youngblom@wisc.edu"
+    handle = Entrez.esearch(db="assembly", term=term, retmax='200')
+    record = Entrez.read(handle)
+    ids = record['IdList']
+    print (f'found {len(ids)} ids')
+    links = []
+    for id in ids:
+        #get summary
+        summary = get_assembly_summary(id)
+        #get ftp link
+        url = summary['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_RefSeq']
+        if url == '':
+            continue
+        label = os.path.basename(url)
+        #get the fasta link - change this to get other formats
+        link = os.path.join(url,label+'_genomic.fna.gz')
+        print (link)
+        links.append(link)
+        if download == True:
+            #download link
+            urllib.request.urlretrieve(link, f'{label}.fna.gz')
+    return links
+
+links = get_assemblies(sys.argv[1], download=True)
