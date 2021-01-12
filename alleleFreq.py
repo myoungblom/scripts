707cd0256e143d0ae765607d98902db29421da31
Madison Youngblom <madisonyoungblom@static-214-209.vpn.wisc.edu>
2021-01-12 09:59:03 -0600

    test

---
+++ alleleFreq.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+
+#####
+# Companion script to alleleFreq.sh, takes in filtered vcf files from
+# a single isolate sequenced at multiple time points, and tabulates
+# mutation frequencies at each point.
+# Filters:
+#   - filters out mutations in regions specified by bed file
+#   - filters out fixed mutations relative to the reference
+#####
+
+if len(sys.argv) < 4:
+	print("Usage: alleleFreq.py <strain.name> <bed file> <vcf1> <vcf2> ... <vcfN")
+	sys.exit(0)
+
+strain = sys.argv[1]
+bed = sys.argv[2]
+vcfs = sys.argv[3:]
+
+
+def getBedCoor(bedfile):
+    bedCoor = []
+    with open(bedfile, "r") as f:
+        for line in f:
+            line = line.strip()
+            info = line.split("\t")
+            for i in range(int(info[1]), int(info[2])+1):
+                bedCoor.append(i)
+    return(bedCoor)
+
+bad_coordinates = getBedCoor(bed)
+
+def filterMuts(vcf, dict_out, filter):
+    with open(vcf,"r") as f:    
+        for line in f:                    
+            if not line.startswith("#"):  
+                line = line.strip()
+                info = line.split("\t")
+                    pos = info[1]
+                    if pos not in filter:
+                        ref = info[3]
+                        alt = info[3]
+                        counts = info[9]
+                        alleles = counts.split(":")[3]
+                        refC = int(alleles.split(",")[0])
+                        altC = int(alleles.split(",")[1])
+                        if (int(alt) > 5):
+                            total = ref+alt
+                            refF = round((ref/total)*100,0)
+                            altF = round((alt/total)*100,0)
+                            dict_out[str(pos)+alt] = [altF]
+    print(dict_out)
+    return dict_out
+
+freqs = {}
+header = ["position","ref","alt"]
+
+for x in vcfs:
+    header.append(x.split(".")[0])
+    filterMuts(x, freqs, bad_coordinates)
