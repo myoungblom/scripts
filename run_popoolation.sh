341849c1b6427e87476d025fb9785ba3edef01fc
Madison Youngblom <madisonyoungblom@static-214-209.vpn.wisc.edu>
2021-01-20 15:29:50 -0600

    first commit

---
+++ run_popoolation.sh
@@ -0,0 +1,24 @@
+#! bin/bash
+
+strain="$1"
+ref="$2"
+bed="$3"
+bams="${@:4}"
+
+# filter bams
+echo "Filtering BAMs ..."
+for x in ${bams};
+do
+    /opt/PepPrograms/samtools-1.11/bin/samtools view -q 20 -b ${x} | /opt/PepPrograms/samtools-1.11/bin/samtools sort - -o ${x/realn/final}
+done
+
+# make mpileup file
+echo "Creating mpileup file ..."
+/opt/PepPrograms/samtools-1.11/bin/samtools mpileup -B -f ${ref} -o ${strain}.mpileup *final.bam
+
+# convert mpileup file to sync
+echo "Converting mpilup file to sync file ..."
+java -ea -Xmx7g -jar /opt/PepPrograms/popoolation2_1201/mpileup2sync.jar --input ${strain}.mpileup --output ${strain}.sync --fastq-type sanger --min-qual 20 --threads 8
+
+# use python script to convert sync file to TSV
+python3 ~/scripts/scripts/popoolationSyncToTSV.py --bed ${bed} ${strain}.sync
