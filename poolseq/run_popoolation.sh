928cbfb4b822be5b918438f9df0a4a6f6316d43f
Madison Youngblom <youngblom@wisc.edu>
2023-05-26 14:29:45 -0500

    minor changes

--- poolseq/run_popoolation.sh
+++ poolseq/run_popoolation.sh
@@ -9,16 +9,9 @@ ref="$2"
 bed="$3"
 bams="${@:4}"
 
-# filter bams
-echo "Filtering BAMs ..."
-for x in ${bams};
-do
-    /opt/PepPrograms/samtools-1.11/bin/samtools view -q 20 -b ${x} | /opt/PepPrograms/samtools-1.11/bin/samtools sort - -o ${x/realn/final}
-done
-
 # make mpileup file
 echo "Creating mpileup file ..."
-/opt/PepPrograms/samtools-1.11/bin/samtools mpileup -B -f ${ref} -o ${strain}.mpileup *final.bam
+/opt/PepPrograms/samtools-1.11/bin/samtools mpileup -B -f ${ref} -o ${strain}.mpileup ${bams}
 
 # convert mpileup file to sync
 echo "Converting mpilup file to sync file ..."
